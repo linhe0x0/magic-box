00fc6be4f58ada0b1aa6e056f606777b5a838e49
sqrtthree <imsqrtthree@gmail.com>
2018-06-15 17:00:31 +0800

    feat(webpack-tts): Add supports for productionGzip and bundleAnalyzerReport
    Signed-off-by: sqrtthree <imsqrtthree@gmail.com>

--- packages/webpack-tts/web/build/webpack.prod.conf.js
+++ packages/webpack-tts/web/build/webpack.prod.conf.js
@@ -10,7 +10,7 @@ const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const baseWebpackConfig = require('./webpack.base.conf')
 const config = require('../config')
 
-module.exports = merge(baseWebpackConfig, {
+const webpackConfig = merge(baseWebpackConfig, {
   devtool: config.build.productionSourceMap ? config.build.devtool : false,
 
   output: {
@@ -74,3 +74,27 @@ module.exports = merge(baseWebpackConfig, {
     ],
   },
 })
+
+if (config.build.productionGzip) {
+  const CompressionWebpackPlugin = require('compression-webpack-plugin')
+
+  webpackConfig.plugins.push(
+    new CompressionWebpackPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: new RegExp(
+        '\\.(' + config.build.productionGzipExtensions.join('|') + ')$'
+      ),
+      threshold: 10240,
+      minRatio: 0.8,
+    })
+  )
+}
+
+if (config.build.bundleAnalyzerReport) {
+  const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+    .BundleAnalyzerPlugin
+  webpackConfig.plugins.push(new BundleAnalyzerPlugin())
+}
+
+module.exports = webpackConfig
