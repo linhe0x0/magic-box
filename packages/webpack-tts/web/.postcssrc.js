152f58a0977430145dba96a2295a8bac0d95fdc5
sqrtthree <imsqrtthree@gmail.com>
2021-05-12 23:22:37 +0800

    chore: Deprecated webpack-tts

--- packages/webpack-tts/web/.postcssrc.js
+++
@@ -1,8 +0,0 @@
-// https://github.com/michael-ciniawsky/postcss-load-config
-module.exports = {
-  plugins: {
-    autoprefixer: {
-      Browserslist: ['> 0.5%, last 4 versions, Firefox ESR, not ie < 8'],
-    },
-  },
-}
